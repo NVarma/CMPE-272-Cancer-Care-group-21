a4ccb454b123785f6d1203cf03873a7b7afe8059
hardikjoshi90 <hardikjoshi90@gmail.com>
2013-12-05 21:37:18 -0800

    java

---
+++ cancercare/src/main/java/com/project/app/Interactive.java
@@ -0,0 +1,55 @@
+package com.project.app;
+
+public class Interactive {
+	
+	private String State;
+	private String Color;
+	private String Rate;	
+	private String LowerCI;
+	private String UpperCI;
+	private String Count;
+	private String population;
+	public String getState() {
+		return State;
+	}
+	public void setState(String state) {
+		State = state;
+	}
+	public String getColor() {
+		return Color;
+	}
+	public void setColor(String color) {
+		Color = color;
+	}
+	public String getRate() {
+		return Rate;
+	}
+	public void setRate(String rate) {
+		Rate = rate;
+	}
+	public String getLowerCI() {
+		return LowerCI;
+	}
+	public void setLowerCI(String lowerCI) {
+		LowerCI = lowerCI;
+	}
+	public String getUpperCI() {
+		return UpperCI;
+	}
+	public void setUpperCI(String upperCI) {
+		UpperCI = upperCI;
+	}
+	public String getCount() {
+		return Count;
+	}
+	public void setCount(String count) {
+		Count = count;
+	}
+	public String getPopulation() {
+		return population;
+	}
+	public void setPopulation(String population) {
+		this.population = population;
+	}
+
+}
---
+++ cancercare/src/main/java/com/project/app/Stories.java
@@ -0,0 +1,18 @@
+package com.project.app;
+
+public class Stories {
+		private String url;
+		private String videoId;
+		public String getUrl() {
+			return url;
+		}
+		public void setUrl(String url) {
+			this.url = url;
+		}
+		public String getVideoId() {
+			return videoId;
+		}
+		public void setVideoId(String videoId) {
+			this.videoId = videoId;
+		}
+}
